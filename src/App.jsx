ba35711033292709faac520ba6d648ec866d608e
Santiago David <santiagodaviduno@hotmail.com>
2022-09-06 04:21:27 -0400

    stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,16 @@
 import { useState } from "react";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
 
-function App() {
-  const [count, setCount] = useState(0);
-
+const App = () => {
   return (
     <div>
-      <p className="motion-safe:animate-bounce">
-        HolaGuapa, Aqui ya tienes todo, solo usa tu imaginacion y recuerda leer
-        la documentacion de Tailwindcss
-      </p>
-      <div className="w-1/4 animate-bounce">
-        <h1 className="font-sans">LEE LA DOCUMENTACION</h1>
-      </div>
+      <Header />
+      <Main />
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div>
+      <h1>{"Get a Try"}</h1>
+    </div>
+  );
+};
+
+export default Header;
